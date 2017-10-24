736898817a354acdcbf13b0326bbcc26b7bd3199
Techfeek <techfreek21@gmail.com>
2017-10-24 14:33:19 -0700

    Help!!!!!!
    I cant get it to count back words

---
+++ tester MK2.py
@@ -0,0 +1,9 @@
+import time
+
+Q1= input("Are you ready fore the lagggggg!!!")
+if Q1 == ('yes'):
+  Q2 = int(input("1.Stars:\n 2.math"))
+  if Q2 == int(1):
+    for t in range (1,6):
+      print (t)
+      time.sleep (1)
